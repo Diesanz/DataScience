8c13b8b8d53f822b90093f439addc363d633bf5e
Diesanz <diego.sanzr2003@gmail.com>
2025-02-18 09:22:12 +0100

    Añdidas carpeta de numpy

---
+++ numpy/arrays_from_pyList.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+#integer array
+np.array([1, 2, 3, 4])
+
+np.array([1, 2, 3, 4], dtype='float32')
+
+#nested lists result un multimensional arrays
+
+np.array([[1, 2], [3, 4]])
+np.array([range(i, i+3) for i in range [2,4,6]])
+
